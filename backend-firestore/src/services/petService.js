4074e2bc173001eb0f48a130662a818844cd9cb5
Akrom <akromsprinter@gmail.com>
2020-06-17 19:03:46 +0900

    Customization 6 firestore

--- backend-firestore/src/services/petService.js
+++ backend-firestore/src/services/petService.js
@@ -1,6 +1,8 @@
 const PetRepository = require('../database/repositories/petRepository');
 const ValidationError = require('../errors/validationError');
 const AbstractRepository = require('../database/repositories/abstractRepository');
+const UserRoleChecker = require('./iam/userRoleChecker');
+const ForbiddenError = require('../errors/forbiddenError');
 
 module.exports = class PetService {
   constructor({ currentUser, language }) {
@@ -10,6 +12,8 @@ module.exports = class PetService {
   }
 
   async create(data) {
+    await this._validateCreate(data);
+
     const batch = await AbstractRepository.createBatch();
 
     try {
@@ -26,7 +30,17 @@ module.exports = class PetService {
     }
   }
 
+  async _validateCreate(data) {
+    if (UserRoleChecker.isPetOwner(this.currentUser)) {
+      if (data.owner !== this.currentUser.id) {
+        throw new ForbiddenError(this.language);
+      }
+    }
+  }
+
   async update(id, data) {
+    await this._validateUpdate(id, data);
+
     const batch = await AbstractRepository.createBatch();
 
     try {
@@ -47,11 +61,20 @@ module.exports = class PetService {
     }
   }
 
+  async _validateUpdate(id, data) {
+    if (UserRoleChecker.isPetOwner(this.currentUser)) {
+      data.owner = this.currentUser.id;
+      await this._validateIsSameOwner(id);
+    }
+  }
+
   async destroyAll(ids) {
     const batch = await AbstractRepository.createBatch();
 
     try {
       for (const id of ids) {
+        await this._validateDestroy(id);
+
         await this.repository.destroy(id, {
           batch,
           currentUser: this.currentUser,
@@ -64,15 +87,58 @@ module.exports = class PetService {
     }
   }
 
-  async findById(id) {
-    return this.repository.findById(id);
+  async _validateDestroy(id) {
+    if (UserRoleChecker.isPetOwner(this.currentUser)) {
+      await this._validateIsSameOwner(id);
+    }
   }
 
-  async findAllAutocomplete(search, limit) {
-    return this.repository.findAllAutocomplete(search, limit);
+  async _validateIsSameOwner(id) {
+    await this.findById(id);
+  }
+
+  async findById(id) {
+    const record = await this.repository.findById(id);
+    await this._validateFindById(record);
+    return record;
+  }
+
+  async _validateFindById(record) {
+    if (UserRoleChecker.isPetOwner(this.currentUser)) {
+      if (
+        record.owner &&
+        record.owner.id !== this.currentUser.id
+      ) {
+        throw new ForbiddenError(this.language);
+      }
+    }
+  }
+
+  async findAllAutocomplete(filter, limit) {
+    if (UserRoleChecker.isPetOwner(this.currentUser)) {
+      if (
+        !filter ||
+        !filter.owner ||
+        filter.owner !== this.currentUser.id
+      ) {
+        throw new ForbiddenError(this.language);
+      }
+    }
+
+    return this.repository.findAllAutocomplete(
+      filter,
+      limit,
+    );
   }
 
   async findAndCountAll(args) {
+    if (UserRoleChecker.isPetOwner(this.currentUser)) {
+      args.filter = {
+        ...args.filter,
+        owner: this.currentUser.id,
+      };
+    }
+
     return this.repository.findAndCountAll(args);
   }
 
